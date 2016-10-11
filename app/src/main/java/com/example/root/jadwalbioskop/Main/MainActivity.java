ed80b805ecf4f0196658fe21f8664ca21663e013
Radhika Yusuf Alifiansyah <radhika.yusuf1@gmail.com>
2016-10-11 11:45:29 +0700

    Second Commit

--- app/src/main/java/com/example/root/jadwalbioskop/Main/MainActivity.java
+++ app/src/main/java/com/example/root/jadwalbioskop/Main/MainActivity.java
@@ -3,13 +3,32 @@ package com.example.root.jadwalbioskop.Main;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.root.jadwalbioskop.Main.ViewModel.MainActivityVM;
 import com.example.root.jadwalbioskop.R;
+import com.example.root.jadwalbioskop.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import id.gits.mvvmcore.activity.GitsActivity;
+
+public class MainActivity extends GitsActivity<MainActivityVM, ActivityMainBinding> {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getToolbarId() {
+        return 0;
+    }
+
+    @Override
+    public int getResLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public MainActivityVM getViewModel() {
+        return new MainActivityVM(this);
+    }
+
+    @Override
+    public void bindViewModel(ActivityMainBinding binding, MainActivityVM viewModel) {
+        binding.setVm(viewModel);
     }
 }
